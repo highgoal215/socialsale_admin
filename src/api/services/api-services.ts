6bbaec9b3e64fee137a50a811ccbf65bde7864e2
Farhan951104 <farhanrizal1104@gmail.com>
2025-07-17 09:26:48 -0700

    changed to localAPI

--- src/api/services/api-services.ts
+++ src/api/services/api-services.ts
@@ -2,8 +2,8 @@
 
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-const API_URL = import.meta.env.VITE_BACKEND_URL || 'https://api.likes.io/api';
-// const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5005/api';
+// const API_URL = import.meta.env.VITE_BACKEND_URL || 'https://api.likes.io/api';
+const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5005/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
